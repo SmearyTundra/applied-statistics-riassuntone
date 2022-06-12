a35970e505bfb71a9a4151698a5f3343c8549142
Teo Bucci <teobucci8@gmail.com>
2022-06-12 13:28:57 +0200

    fine lab 14
    boh sta roba è arabo

--- miscellaneous/teo/14 FDA.R
+++ miscellaneous/teo/14 FDA.R
@@ -2,6 +2,102 @@
 # FUNCTIONAL
 #########
 
+#### Reggression splines
+library(fda)
+
+
+norder <- 5      # spline order (4th order polynomials)
+degree <- m-1    # spline degree
+nbasis <- 9      # how many basis we want
+
+basis <- create.bspline.basis(rangeval=c(0, 1),
+                              nbasis=nbasis,
+                              norder=norder)
+
+basismat <- eval.basis(abscissa, basis)
+est_coef <- lsfit(basismat, Xobs0, intercept=FALSE)$coef
+Xsp0 <- basismat %*% est_coef
+
+# plot fitted line
+plot(abscissa, Xobs0, xlab="t", ylab="observed data")
+points(abscissa, Xsp0, type="l", col="blue", lwd=2)
+abline(v=basis$params)
+# 1st derivative
+basismat1 <- eval.basis(abscissa, basis, Lfdobj=1)
+Xsp1 <- basismat1 %*% est_coef
+# 2nd derivative
+basismat2 <- eval.basis(abscissa, basis, Lfdobj=2)
+Xsp2 <- basismat2 %*% est_coef
+
+
+# faster
+Xsp <- smooth.basis(argvals=abscissa, y=Xobs0, fdParobj=basis)
+Xsp0bis <- eval.fd(abscissa, Xsp$fd) #  the curve smoothing the data
+Xsp1bis <- eval.fd(abscissa, Xsp$fd, Lfd=1) # first derivative
+Xsp2bis <- eval.fd(abscissa, Xsp$fd, Lfd=2) # second derivative
+
+
+
+# basis choice with cross validation
+nbasis <- 6:30
+gcv <- numeric(length(nbasis))
+for (i in 1:length(nbasis)){
+    basis <- create.bspline.basis(c(0, 1), nbasis[i], m)
+    gcv[i] <- smooth.basis(abscissa, Xobs0, basis)$gcv
+}
+par(mfrow=c(1, 1))
+plot(nbasis, gcv)
+nbasis[which.min(gcv)]
+abline(v=nbasis[which.min(gcv)], col=2)
+
+
+# specify breaks
+breaks <- abscissa
+basis <- create.bspline.basis(breaks, norder=m)
+functionalPar <- fdPar(fdobj=basis, Lfdobj=3, lambda=1e-8) # try changing lambda
+                                                            # 1e-5 oversmoothing
+                                                            # 1e-12 overfit
+Xss <- smooth.basis(abscissa, Xobs0, functionalPar)
+Xss0 <- eval.fd(abscissa, Xss$fd, Lfd=0)
+Xss1 <- eval.fd(abscissa, Xss$fd, Lfd=1)
+Xss2 <- eval.fd(abscissa, Xss$fd, Lfd=2)
+
+# and use CV to set lambda
+lambda <- 10^seq(-12, -5, by=0.5)
+gcv <- numeric(length(lambda))
+for (i in 1:length(lambda)){
+    functionalPar <- fdPar(fdobj=basis, Lfdobj=3, lambda=lambda[i])
+    gcv[i] <- smooth.basis(abscissa, Xobs0, functionalPar)$gcv
+}
+par(mfrow=c(1, 1))
+plot(log10(lambda), gcv)
+lambda[which.min(gcv)]
+
+# substitute it
+functionalParbest <- fdPar(fdobj=basis, Lfdobj=3, lambda=lambda[which.min(gcv)])
+Xssbest <- smooth.basis(abscissa, Xobs0, functionalParbest)
+Xss0best <- eval.fd(abscissa, Xssbest$fd, Lfd=0)
+Xss1best <- eval.fd(abscissa, Xssbest$fd, Lfd=1)
+Xss2best <- eval.fd(abscissa, Xssbest$fd, Lfd=2)
+
+
+
+
+#### Local Polynomial Regression
+library(KernSmooth)
+# in lab 14 copia tutto
+
+
+
+
+
+
+
+
+
+
+
+
 ##### FPCA - BSPLINE + HARMONICS + SCORES  ####
 rm(list = ls())
 graphics.off()
@@ -67,17 +163,35 @@ wl <- 1:80
 data_W.fd.1 <- Data2fd(y = spectra,argvals = wl,basisobj = basis)
 plot.fd(data_W.fd.1)
 
+
+
+
+# kmean alignment
+
 library(fdakma)
-fdakma_example_noalign_0der <- kma(
+fdakma_example <- kma(
   x=1:80, y0=t(spectra), n.clust = 3, 
-  warping.method = 'affine', 
-  similarity.method = 'd0.pearson',   # similarity computed as the cosine
+  warping.method = 'NOalignment-affine', 
+  similarity.method = 'd0.pearson-d1.pearson',   # similarity computed as the cosine
   # between the original curves 
   # (correlation)
+  # similarity related to which derivative
   center.method = 'k-means'
   #,seeds = c(1,11,21) # you can give a little help to the algorithm...
 )
 
-kma.show.results(fdakma_example_noalign_0der)
-fdakma_example_noalign_0der$labels
+kma.show.results(fdakma_example)
+fdakma_example$labels
 
+#fdakma_example$shift
+#fdakma_example$dilation
+
+# compare different warping and cluster
+# plotta in automatico
+kma.compare_example_3 <- kma.compare (
+ x=x, y0=y0, y1=y1, n.clust=1:3,
+ warping.method=c("NOalignment", "shift", "dilation", "affine"),
+ similarity.method='d1.pearson',
+ center.method='k-means',
+ seeds=c(1, 21, 30),
+ plot.graph=TRUE)
